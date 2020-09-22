a6d3856cfd5332862cc4c63265af7fd6f16d8f1c
Harsh Biyani <harshbiyani07@gmail.com>
2020-09-22 08:05:25 +0530

    Added date filter to home.html

--- blog_project/blog/blog_app/admin.py
+++ blog_project/blog/blog_app/admin.py
@@ -3,6 +3,6 @@ from . import models
 
 
 class PostAdmin(admin.ModelAdmin):
-    list_display = ['id', 'title', 'content', 'date_posted', 'user']
+    list_display = ['id', 'title', 'content', 'date_posted']
 
 admin.site.register(models.Post, PostAdmin)
--- blog_project/blog/blog_app/views.py
+++ blog_project/blog/blog_app/views.py
@@ -1,26 +1,28 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Post
+
 
 # Create your views here.
-posts = [
-    {
-        'author': 'Harsh Biyani',
-        'title': 'Blog Post 1',
-        'content': 'First post content',
-        'date_posted': 'September 19, 2020'
-    },
-    {
-        'author': 'Harshita Biyani',
-        'title': 'Blog Post 2',
-        'content': 'Second post content',
-        'date_posted': 'September 20, 2020'
-    },
-]
+# posts = [
+#     {
+#         'author': 'Harsh Biyani',
+#         'title': 'Blog Post 1',
+#         'content': 'First post content',
+#         'date_posted': 'September 19, 2020'
+#     },
+#     {
+#         'author': 'Harshita Biyani',
+#         'title': 'Blog Post 2',
+#         'content': 'Second post content',
+#         'date_posted': 'September 20, 2020'
+#     },
+# ]
 
 def home(request):
     """ Displays the home page consisting all posts """
-    context = {'posts': posts}
-    return render(request, 'blog_app/home.html', context=context)
+    posts = Post.objects.all()
+    return render(request, 'blog_app/home.html', {'posts': posts})
 
 
 def about(request):
