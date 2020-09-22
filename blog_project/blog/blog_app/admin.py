92ee50d21f42e469c3185e837de076e82eabdba2
Harsh Biyani <harshbiyani07@gmail.com>
2020-09-22 07:29:58 +0530

    created Post Model

--- blog_project/blog/blog_app/admin.py
+++ blog_project/blog/blog_app/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from . import models
 
-# Register your models here.
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['id', 'title', 'content', 'date_posted', 'user']
+
+admin.site.register(models.Post, PostAdmin)
