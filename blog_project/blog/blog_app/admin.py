fa6585f67ab70d2a346a91372a5a3bfc6499c03f
Harsh Biyani <Harsh.Biyani@T-Mobile.com>
2021-07-11 19:59:34 +0530

    removed blog project

--- blog_project/blog/blog_app/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from . import models
-
-
-class PostAdmin(admin.ModelAdmin):
-    list_display = ['id', 'title', 'content', 'date_posted']
-
-admin.site.register(models.Post, PostAdmin)
