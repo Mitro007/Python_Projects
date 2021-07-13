ac6303cc7ddab7aefed799015445163a9f2a1f18
Harsh Biyani <Harsh.Biyani@T-Mobile.com>
2021-07-13 09:23:48 +0530

    Template Inheritance

--- blogging-project-django/blogging_app/blog/views.py
+++ blogging-project-django/blogging_app/blog/views.py
@@ -3,11 +3,27 @@ from django.http import HttpResponse
 
 # FBV - always return HttpResponse or Error
 
+posts = [
+    {
+        'author': 'CoreyMS',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'August 27, 2018'
+    },
+    {
+        'author': 'Jane Doe',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'August 28, 2018'
+    }
+]
+
 def greet(request):
   return HttpResponse('Hello Django!!')
 
 def home(request):
-  return render(request, 'index.html')
+  context = {'posts': posts,}
+  return render(request, 'home.html', context=context)
 
 def about(request):
-  return render(request, 'about.html')
+  return render(request, 'about.html', {'title': 'About'})
