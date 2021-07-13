d6d587fa42a2858bb6a7823dea561ed4f7ae42a1
Harsh Biyani <Harsh.Biyani@T-Mobile.com>
2021-07-13 11:49:35 +0530

    Database interaction using Models and filters in HTML

--- blogging-project-django/blogging_app/blog/models.py
+++ blogging-project-django/blogging_app/blog/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
 
-class Posts(models.Model):
+class Post(models.Model):
   author = models.ForeignKey(User, on_delete=models.CASCADE)
   title = models.CharField(max_length=100)
   content = models.TextField()
--- blogging-project-django/blogging_app/blog/views.py
+++ blogging-project-django/blogging_app/blog/views.py
@@ -1,28 +1,14 @@
+from .models import Post
 from django.shortcuts import render
 from django.http import HttpResponse
 
 # FBV - always return HttpResponse or Error
 
-posts = [
-    {
-        'author': 'CoreyMS',
-        'title': 'Blog Post 1',
-        'content': 'First post content',
-        'date_posted': 'August 27, 2018'
-    },
-    {
-        'author': 'Jane Doe',
-        'title': 'Blog Post 2',
-        'content': 'Second post content',
-        'date_posted': 'August 28, 2018'
-    }
-]
-
 def greet(request):
   return HttpResponse('Hello Django!!')
 
 def home(request):
-  context = {'posts': posts,}
+  context = {'posts': Post.objects.all(),}
   return render(request, 'home.html', context=context)
 
 def about(request):
